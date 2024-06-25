5ebc5cbd6942dda8faf6c643a49d7fd170a3329b
Manuel Folgar <manuel.folgarberrios@csedge.org>
2024-06-25 14:08:20 -0700

    madlib version V1

--- madlibs.py
+++ madlibs.py
@@ -3,6 +3,8 @@
 print("Here is my first commit :) :) ")
 
 name = input("please enter a name")
+pronoun = input("he or her")
+pronoun2 = input("his or hers")
 adjective = input("please enter an adjective")
 noun = input("please enter a place")
 adverb = input("please enter an adverb")
@@ -11,4 +13,4 @@ color_plural = input("please enter a color")
 plural = input("please enter a number")
 day = input("enter a time of day")
 
-print(f"once upon a time {name} with a {color_plural}shirt was {verb}during  {plural}degree weather \n and was  feeling {adjective} because they were doing it at {noun}during {day}\nbut they were doing it {adverb}because they were being paid by their parents") 
+print(f"once upon a time {name} with a {color_plural} shirt was {verb} during  {plural} degree weather \n and {pronoun} was  feeling {adjective} because {pronoun} was doing it at {noun}during {day}\nbut {pronoun} were doing it {adverb} because {pronoun} were being paid by {pronoun2} parents") 
