130d2157334ee25f63917082bf93ad6ce36113b8
Manuel Folgar <manuel.folgarberrios@csedge.org>
2024-06-25 13:50:48 -0700

    Creeated Malib V1

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,14 @@
 #MadLib by Coach Olivia
 
 print("Here is my first commit :) :) ")
+
+name = input("please enter a name")
+adjective = input("please enter an adjective")
+noun = input("please enter a place")
+adverb = input("please enter an adverb")
+verb = input("please enter a verb")
+color_plural = input("please enter a color") 
+plural = input("please enter a number")
+day = input("enter a time of day")
+
+print(f"once upon a time {name} with a {color_plural}shirt was {verb}during  {plural}degree weather \n and was  feeling {adjective} because they were doing it at {noun}during {day}\nbut they were doing it {adverb}because they were being paid by their parents") 
